211e63b971a02aeb35c889994eebc445a47fb646
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2026-05-26 18:26:50 +0700

    fix

---
+++ resources/views/layouts/owner.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Salesight Owner</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/global.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owner-sidebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owner-header.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owner-dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owner-tren-global.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/tren-penjualan-toko.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owner-kontribusi-toko.css') }}">
+</head>
+<body>
+    <div class="layout-wrapper">
+        @include('sidebar.owner-sidebar')
+        <div class="main-content">
+            @include('header.owner-header')
+            <main>
+                @yield('content')
+            </main>
+        </div>
+    </div>
+</body>
+</html>
